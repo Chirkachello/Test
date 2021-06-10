1c4ab9f7865d0b16e3778c7dbd9385d6b95a8b20
Vitalik Chirkin <vitalikchirkin@gmail.com>
2021-06-10 16:33:17 +0300

    add test

---
+++ tests1212/conf/userproperties.js
@@ -0,0 +1,11 @@
+
+class UserProperties {
+
+    username = 'Chirkachell'
+    wrongUserName = 'TestuserTest'
+    wrongUserPassword = 'wrongpassword123067'
+    password = '17351735'
+
+}
+
+module.exports = new UserProperties()
